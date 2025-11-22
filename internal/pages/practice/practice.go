d9e3df4832df2bf76aac49dcd34b604149f6f713
Iztuk <johnandrew.kutz@gmail.com>
2025-11-22 15:58:05 -0600

    Updated the logic for rendering correct/wrong input colors

--- internal/pages/practice/practice.go
+++ internal/pages/practice/practice.go
@@ -1,17 +1,24 @@
 package practice
 
 import (
-	"fmt"
 	"strings"
 
 	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
 )
 
 type PracticeModel struct {
-	TargetText   []string
+	TargetText   []Glyph
 	Input        []string
 	CurrentIndex int
 }
+
+var (
+	correctStyle = lipgloss.NewStyle().
+			Foreground(lipgloss.Color("82"))
+	wrongStyle = lipgloss.NewStyle().
+			Foreground(lipgloss.Color("203"))
+)
 
 func InitialPracticeModel() PracticeModel {
 	return PracticeModel{
@@ -30,7 +37,14 @@ func (m PracticeModel) Update(msg tea.Msg) (PracticeModel, tea.Cmd) {
 				m.CurrentIndex--
 			}
 		default:
+			if len(k) != 1 {
+				return m, nil
+			}
+
 			m.Input = append(m.Input, k)
+
+			m.TargetText[m.CurrentIndex] = evaluateInput(k, m.TargetText[m.CurrentIndex])
+
 			m.CurrentIndex++
 		}
 	}
@@ -38,6 +52,20 @@ func (m PracticeModel) Update(msg tea.Msg) (PracticeModel, tea.Cmd) {
 }
 
 func (m PracticeModel) View() string {
-	text := fmt.Sprintf("%v %s", strings.Join(m.TargetText, " "), m.Input)
-	return text
+	var b strings.Builder
+
+	for _, g := range m.TargetText {
+		ch := string(g.Char)
+
+		switch g.State {
+		case 1:
+			b.WriteString(correctStyle.Render(ch))
+		case 2:
+			b.WriteString(wrongStyle.Render(ch))
+		default:
+			b.WriteString(ch)
+		}
+	}
+
+	return b.String()
 }
--- internal/pages/practice/session.go
+++ internal/pages/practice/session.go
@@ -1,17 +1,46 @@
 package practice
 
-import "math/rand"
+import (
+	"math/rand"
+)
+
+type Glyph struct {
+	Char  string
+	State GlyphState
+}
+
+type GlyphState int
+
+const (
+	Pending GlyphState = iota
+	Correct
+	Wrong
+)
 
 var (
 	lowerCaseLetters = []string{"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"}
 )
 
-func createSessionText() []string {
-	var sessionText []string
+func createSessionText() []Glyph {
+	var sessionText []Glyph
 
 	for _ = range 100 {
-		sessionText = append(sessionText, lowerCaseLetters[rand.Intn(25)])
+		var text = Glyph{
+			Char:  lowerCaseLetters[rand.Intn(25)],
+			State: 0,
+		}
+		sessionText = append(sessionText, text)
 	}
 
 	return sessionText
+}
+
+// Compares Input vs Target, returns the changed Glyph state (correct or wrong)
+func evaluateInput(i string, g Glyph) Glyph {
+	if i == g.Char {
+		g.State = 1
+	} else {
+		g.State = 2
+	}
+	return g
 }
